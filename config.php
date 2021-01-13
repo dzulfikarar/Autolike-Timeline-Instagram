35c22c5e6c9fe12ee6faab1342500885b07c925a
dzulfikarar <77370200+dzulfikarar@users.noreply.github.com>
2021-01-13 19:08:31 +0700

    Update config.php

--- config.php
+++ config.php
@@ -20,8 +20,9 @@ $FADILUS=array(
 		/*
 			SILAHKAN CING DI MASUKIN PELAN PELAN :V
 		*/
-		'', // USERNAME instagram lu
-		'', // PASSWORD instagram lu
+		'', // pikardih
+		'', // baladewa2020
+
 		/* JANGAN DI EDIT.. !!! */
 		'https://i.instagram.com/api/v1/', 'Instagram 9.0.1 Android (18/4.3; 320dpi; 720x1280; Xiaomi; HM 1SW; armani; qcom; en_US)', '96724bcbd4fb3e608074e185f2d4f119156fcca061692a4a1db1c7bf142d3e22'
 	),
